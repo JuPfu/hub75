5a9c7b7438f48cf5efb4c3bc2a7c30f9a5c00071
Juergen Pfundt <juergen.pfundt@gmail.com>
2026-01-11 15:05:39 +0100

    moved fm6126a into separate file

--- hub75.cpp
+++ hub75.cpp
@@ -11,6 +11,7 @@
 #include "hub75.pio.h"
 
 #include "rul6024.h"
+#include "fm6126a.h"
 
 // Deduced from https://jared.geek.nz/2013/02/linear-led-pwm/
 // The CIE 1931 lightness formula is what actually describes how we perceive light.
@@ -68,10 +69,6 @@ static void setup_dma_irq();
 static volatile uint32_t dummy_pixel_data[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 // Control data for the output enable signal - keep volatile!
 static volatile uint32_t oen_finished_data = 0;
-
-const bool clk_polarity = 1;
-const bool stb_polarity = 1;
-const bool oe_polarity = 0;
 
 // Width and height of the HUB75 LED matrix
 static uint width;
@@ -285,124 +282,6 @@ void start_hub75_driver()
 #elif defined(HUB75_P10_3535_16X32_4S) || defined(HUB75_P3_1415_16S_64X64)
     dma_channel_set_read_addr(pixel_chan, &frame_buffer[row_address * (width << 2)], true);
 #endif
-}
-
-void FM6126A_init_register()
-{
-    // Set up GPIO
-    gpio_init(DATA_BASE_PIN);
-    gpio_set_function(DATA_BASE_PIN, GPIO_FUNC_SIO);
-    gpio_set_dir(DATA_BASE_PIN, true);
-    gpio_put(DATA_BASE_PIN, 0);
-    gpio_init((DATA_BASE_PIN + 1));
-    gpio_set_function((DATA_BASE_PIN + 1), GPIO_FUNC_SIO);
-    gpio_set_dir((DATA_BASE_PIN + 1), true);
-    gpio_put((DATA_BASE_PIN + 1), 0);
-    gpio_init((DATA_BASE_PIN + 2));
-    gpio_set_function((DATA_BASE_PIN + 2), GPIO_FUNC_SIO);
-    gpio_set_dir((DATA_BASE_PIN + 2), true);
-    gpio_put((DATA_BASE_PIN + 2), 0);
-
-    gpio_init((DATA_BASE_PIN + 3));
-    gpio_set_function((DATA_BASE_PIN + 3), GPIO_FUNC_SIO);
-    gpio_set_dir((DATA_BASE_PIN + 3), true);
-    gpio_put((DATA_BASE_PIN + 3), 0);
-    gpio_init((DATA_BASE_PIN + 4));
-    gpio_set_function((DATA_BASE_PIN + 4), GPIO_FUNC_SIO);
-    gpio_set_dir((DATA_BASE_PIN + 4), true);
-    gpio_put((DATA_BASE_PIN + 4), 0);
-    gpio_init((DATA_BASE_PIN + 5));
-    gpio_set_function((DATA_BASE_PIN + 5), GPIO_FUNC_SIO);
-    gpio_set_dir((DATA_BASE_PIN + 5), true);
-    gpio_put((DATA_BASE_PIN + 5), 0);
-
-    gpio_init(ROWSEL_BASE_PIN);
-    gpio_set_function(ROWSEL_BASE_PIN, GPIO_FUNC_SIO);
-    gpio_set_dir(ROWSEL_BASE_PIN, true);
-    gpio_put(ROWSEL_BASE_PIN, 0);
-    gpio_init((ROWSEL_BASE_PIN + 1));
-    gpio_set_function((ROWSEL_BASE_PIN + 1), GPIO_FUNC_SIO);
-    gpio_set_dir((ROWSEL_BASE_PIN + 1), true);
-    gpio_put((ROWSEL_BASE_PIN + 1), 0);
-    gpio_init((ROWSEL_BASE_PIN + 2));
-    gpio_set_function((ROWSEL_BASE_PIN + 2), GPIO_FUNC_SIO);
-    gpio_set_dir((ROWSEL_BASE_PIN + 2), true);
-    gpio_put((ROWSEL_BASE_PIN + 2), 0);
-    gpio_init((ROWSEL_BASE_PIN + 3));
-    gpio_set_function((ROWSEL_BASE_PIN + 3), GPIO_FUNC_SIO);
-    gpio_set_dir((ROWSEL_BASE_PIN + 3), true);
-    gpio_put((ROWSEL_BASE_PIN + 3), 0);
-    gpio_init((ROWSEL_BASE_PIN + 4));
-    gpio_set_function((ROWSEL_BASE_PIN + 4), GPIO_FUNC_SIO);
-    gpio_set_dir((ROWSEL_BASE_PIN + 4), true);
-    gpio_put((ROWSEL_BASE_PIN + 4), 0);
-
-    gpio_init(CLK_PIN);
-    gpio_set_function(CLK_PIN, GPIO_FUNC_SIO);
-    gpio_set_dir(CLK_PIN, true);
-    gpio_put(CLK_PIN, !clk_polarity);
-    gpio_init(STROBE_PIN);
-    gpio_set_function(STROBE_PIN, GPIO_FUNC_SIO);
-    gpio_set_dir(STROBE_PIN, true);
-    gpio_put(CLK_PIN, !stb_polarity);
-    gpio_init(OEN_PIN);
-    gpio_set_function(OEN_PIN, GPIO_FUNC_SIO);
-    gpio_set_dir(OEN_PIN, true);
-    gpio_put(CLK_PIN, !oe_polarity);
-}
-
-void FM6126A_write_register(uint16_t value, uint8_t position)
-{
-    gpio_put(OEN_PIN, HIGH);
-    gpio_put(CLK_PIN, LOW);
-    gpio_put(STROBE_PIN, LOW);
-
-    sleep_ms(10);
-
-    uint8_t threshold = width - position;
-    for (auto i = 0u; i < width; i++)
-    {
-        auto j = i % 16;
-        bool b = value & (1 << j);
-
-        gpio_put(DATA_BASE_PIN, b);
-        gpio_put((DATA_BASE_PIN + 1), b);
-        gpio_put((DATA_BASE_PIN + 2), b);
-        gpio_put((DATA_BASE_PIN + 3), b);
-        gpio_put((DATA_BASE_PIN + 4), b);
-        gpio_put((DATA_BASE_PIN + 5), b);
-
-        // Assert strobe/latch if i > threshold
-        // This somehow indicates to the FM6126A which register we want to write :|
-        gpio_put(STROBE_PIN, i > threshold);
-        gpio_put(CLK_PIN, HIGH);
-        sleep_ms(10);
-        gpio_put(CLK_PIN, LOW);
-    }
-    gpio_put(OEN_PIN, LOW);
-}
-
-/**
- * @brief Generate initialisation sequence for FM6126A based led matrix panels.
- *
- * First initialise all GPIOs connected to the led matrix panel.
- * Second send the initialisation sequence to the FM6126A based led matrix panel.
- * The source code is based on Pimoronis Hub75 driver, see https://github.com/pimoroni/pimoroni-pico/blob/main/drivers/hub75/hub75.cpp
- *
- */
-void FM6126A_setup()
-{
-    FM6126A_init_register();
-
-    // Ridiculous register write nonsense for the FM6126A-based 64x64 matrix
-    FM6126A_write_register(0b1111111111111110, 12);
-    FM6126A_write_register(0b0000010000000000, 13);
-
-    // FM6126A_write_register(0b1111111111000000, 12);
-    // FM6126A_write_register(0b0000000001000000, 13);
-
-    // FM6126A_write_register(WREG1, 12);
-    // FM6126A_write_register(WREG2, 13);
 }
 
 /**
