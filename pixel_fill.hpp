693faf2fe6c78c5fae6410b1de58d7a4fc1fb0a1
Juergen Pfundt <juergen.pfundt@gmail.com>
2026-01-20 20:38:27 +0100

    line colours modified

--- pixel_fill.hpp
+++ pixel_fill.hpp
@@ -12,10 +12,9 @@ private:
 
     volatile int i = 0;
     volatile int j = 0;
+    volatile int l = 0;
 
     volatile int index = 0;
-    volatile int l = 0;
-    volatile uint32_t c = 0;
 
     void drawPixel(int x, int y, uint32_t color)
     {
@@ -29,89 +28,25 @@ public:
         set_pen(0);
         clear();
         setIntensity(1.0);
-
-        printf("PixelFill w=%d  h=%d\n", w, h);
     }
 
     void fill(int start, int end)
     {
-        static const uint32_t col[] = {0xFF0000, 0x00FF00, 0x0000FF, 0xa06090, 0xFFFF00, 0xFF00FF, 0xF0F0F0, 0xFFAA55,
-                                       0x00F00F, 0x00F0FF, 0x00FFFF, 0x0F0000, 0x0F000F, 0x0F00F0, 0x0F00FF, 0x0F0F00};
+        static const uint32_t col[] = {0xFF0000, 0x00FF00, 0x0000FF, 0xBE2633, 0xE06F8B, 0x493C2B, 0xA46422, 0xEB8931,
+                                       0xF7E26B, 0x2F484E, 0x44891A, 0xA3CE27, 0x1B2632, 0x005784, 0x31A2F2, 0xB2DCEF};
+
+        drawPixel(j++, l, col[index]);
 
         if (j >= w)
         {
             j = 0;
+            l++;
             if (l >= h)
                 l = 0;
-            l++;
+            if ((l % 2) == 0)
+                index++;
+            if (index >= 16)
+                index = 0;
         }
-
-        if ((i) < 32)
-        {
-            drawPixel(j, l, col[0]); // red
-        }
-        else if (i < 64)
-        {
-            drawPixel(j, l, col[1]); // green
-        }
-        else if (i < 96)
-        {
-            drawPixel(j, l, col[3]); // brown
-        }
-        else if (i < 128)
-        {
-            drawPixel(j, l, col[4]); // yellow
-        }
-        else if (i < 5 * w)
-        {
-            drawPixel(j, l, col[6]); // grey
-        }
-        else if (i < 6 * w)
-        {
-            drawPixel(j, l, col[2]); // blue
-        }
-        else if (i < 7 * w)
-        {
-            drawPixel(j, l, col[0]); // red
-        }
-        else if (i < 8 * w)
-        {
-            drawPixel(j, l, col[1]); // green
-        }
-        else if (i < 9 * w)
-        {
-            drawPixel(j, l, col[2]); // blue
-        }
-        else if (i < 10 * w)
-        {
-            drawPixel(j, l, col[3]); // brown
-        }
-        else if (i < 11 * w)
-        {
-            drawPixel(j, l, col[4]); // brown
-        }
-        else if (i < 12 * w)
-        {
-            drawPixel(j, l, col[5]); // brown
-        }
-        else if (i < 13 * w)
-        {
-            drawPixel(j, l, col[6]); // brown
-        }
-        else if (i < 14 * w)
-        {
-            drawPixel(j, l, col[7]); // brown
-        }
-        else if (i < 15 * w)
-        {
-            drawPixel(j, l, col[8]); // brown
-        }
-        else
-        {
-            drawPixel(j, l, col[9]); // brown
-        }
-        j++;
-        i++;
-        if ( i >= 512 ) i = 0;
     }
 };
