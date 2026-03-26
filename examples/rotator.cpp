3925e580352409d51cc941a51c88147d11f2ff7a
Juergen Pfundt <juergen.pfundt@gmail.com>
2026-03-26 13:11:44 +0100

    small performance improvement

--- examples/rotator.cpp
+++ examples/rotator.cpp
@@ -15,14 +15,17 @@ private:
     float rx = 1.0f;
     float ry = 1.0f;
     float r = 1.0f;
-    float a = 0.0f;
     float da = M_PI / 180.0f;
-    float incr = 0.0f;
+
+    float *s;
+    float *c;
 
     float centerX = 0.0f;
     float centerY = 0.0f;
 
-    uint32_t col = 0xffff00;
+    int j = 0;
+
+    uint32_t col = 0xffffff;
 
 public:
     explicit Rotator(uint width = 64, uint height = 64) : AntialiasedLine(width, height), w(width), h(height)
@@ -32,7 +35,17 @@ public:
 
         uint l = MIN(w, h);
 
-        r = l / 2.0f;
+        r = l / 2.0f - 1.01f;
+
+        s = new float[360]();
+        c = new float[360]();
+        for (auto i = 180; i < 540; i++)
+        {
+            s[i - 180] = std::sin(i * da) * r;
+            c[i - 180] = std::cos(i * da) * r;
+        }
+
+        j = 0;
     }
 
     void draw()
@@ -41,18 +54,26 @@ public:
         clear();
         set_pen(col);
 
-        float c = cos(a);
-        float s = sin(a);
+        rx = c[j];
+        ry = s[j];
 
-        r = std::clamp(r + incr, 0.0f, 30.0f);
-
-        rx = r * c;
-        ry = r * s;
-
-        a += da;
-        if (a >= 2.0f * M_PI)
-            a -= 2.0f * M_PI;
-
-        drawLine(rx + centerX, ry + centerY, -rx + centerX, -ry + centerY, col);
+        if (j > 270)
+        {
+            drawLine(rx + centerX, ry + centerY, -rx + centerX, -ry + centerY, 0xffff00);
+        }
+        else if (j > 180)
+        {
+            drawLine(rx + centerX, ry + centerY, -rx + centerX, -ry + centerY, 0x00ff00);
+        }
+        else if (j > 90)
+        {
+            drawLine(rx + centerX, ry + centerY, -rx + centerX, -ry + centerY, 0xff0000);
+        }
+        else
+        {
+            drawLine(rx + centerX, ry + centerY, -rx + centerX, -ry + centerY, col);
+        }
+        if (++j >= 360)
+            j = 0;
     }
 };
